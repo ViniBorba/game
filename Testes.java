4680cbbd126a1ce127c56f27824bb5a57539b806
Vinicius Borba <vinichius@gmail.com>
2017-09-25 11:45:59 -0300

    Sorteio do dado, não precisa do array, o Random funciona

--- Testes.java
+++ Testes.java
@@ -2,23 +2,54 @@ import java.util.Random;
 
 class Dados {
 
-	public static void main(String[] args) {
+
+
+			public static void main(String[] args) {
+
+
+
+				Dados d6 = new Dados();
+
+
+				for(int i = 0; i < 50; i++){	
+				System.out.println(d6.sorteiaDado());
+				}
+			
+			}
+	
+
+
+
+
 
 	//int é o objeto do array dado6
-	int[] dado6  = new int[6];
+	 int sorteiaDado(){
 
-	int aux = 1;
-	Random radom  = new Random();
+		//int[] dado6  = new int[6];
 
-	aux = 1 + radom.nextInt(6);
+		int aux = 1;
+		Random radom  = new Random();
 
-	System.out.println(aux);
-	
-	for(int i = 0; i < 6; i++){
-		 dado6[i] = i +1 ;
+		return aux = 1 + radom.nextInt(6);
+
+		//System.out.println(aux);
+		
+		//for(int i = 0; i < 6; i++){
+		//	 dado6[i] = i +1 ;
+		//}
+
 	}
 
-	
+	void muda(){
+		System.out.println("Olá");
+	}
+
+
+
+
+
+
+
 }
-	
-}
+
+
