b6151b48f30cde90fc0282ca66161c6ebb86dc08
Vinicius Borba <vinichius@gmail.com>
2017-09-25 11:18:29 -0300

    Dados d6 funcionando e sorteado

--- Testes.java
+++ Testes.java
@@ -1,10 +1,24 @@
+import java.util.Random;
 
 class Dados {
 
+	public static void main(String[] args) {
+
 	//int é o objeto do array dado6
-	private int[] dado6  = new int[6];
+	int[] dado6  = new int[6];
+
+	int aux = 1;
+	Random radom  = new Random();
+
+	aux = 1 + radom.nextInt(6);
+
+	System.out.println(aux);
+	
+	for(int i = 0; i < 6; i++){
+		 dado6[i] = i +1 ;
+	}
 
 	
-
+}
 	
 }
